03ba71c3b8f14ad2f9ce5751220bec4999383eca
Sametakkaya <burak_samet19@hotmail.com>
2022-11-03 12:13:20 +0300

    Producton & Development Ortamı

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
+import logo from './logo.svg'
 function App() {
   return (
-    <div className="App">
-      Sayfada Bu yazıyı gösterir
+    <div className='App'>
+      <h3>{process.env.NODE_ENV}</h3>
+      {process.env.NODE_ENV === 'production' && (
+        <>
+          <h3>Bu Başlık Gözükmez</h3>
+          <p>Bu Paragraf Gözükmez</p>
+          <img src={logo} alt='' />
+        </>
+      )}
+      <h4>Ama bu başlık gözükür</h4>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
