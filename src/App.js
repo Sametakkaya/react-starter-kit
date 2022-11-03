affa2d480d1318c85393569f047b680d8492edba
Sametakkaya <burak_samet19@hotmail.com>
2022-11-03 15:10:58 +0300

    .env development & production

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import logo from './logo.svg'
+
 function App() {
   return (
     <div className='App'>
       <h3>{process.env.NODE_ENV}</h3>
+      <p>{process.env.REACT_APP_API_URL}</p>
       {process.env.NODE_ENV === 'production' && (
         <>
           <h3>Bu Başlık Gözükmez</h3>
